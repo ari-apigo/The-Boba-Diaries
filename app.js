5d2c664927d07b76d7355e610b5cfb0250ee71ec
arianea <arianea@uw.edu>
2019-11-08 10:31:28 -0800

    create all necessary files

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+ *  Name: Ariane Apigo
+ *  Date: 2019 November 8
+ *  Section: AA / Chao Hsu Lin & Austin Jenchi
+ *
+ *  This is the app.js file for my
+ */
+
+"use strict";
+
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+ *  Name: Ariane Apigo
+ *  Date: 2019 November 8
+ *  Section: AA / Chao Hsu Lin & Austin Jenchi
+ *
+ *  This is the index.js file for my
+ */
+
+"use strict";
+(function() {
+
+
+  window.addEventListener("load", init);
+
+  /**
+   *
+   */
+  function init() {
+
+  }
+
+  })();
