d0592844828547ac66b64ced3549bc692601d24e
arianea <arianea@uw.edu>
2019-11-08 22:38:48 -0800

    figuring out json endpoint

--- app.js
+++ app.js
@@ -9,7 +9,41 @@
 "use strict";
 
 const express = require("express");
+const fs = require("fs").promises;
 const app = express();
 
+// shows list of franchise names and their names as used to navigate API
+app.get("/franchises", async function (req, res) {
+  let franchises = await getTxtFile();
+  res.send(franchises);
+})
+
+// shows whole franchiseInfo file
+app.get("/franchiseInfo", async function (req, res) {
+  let franchiseInfo = await getJsonFile();
+  res.send(franchiseInfo);
+})
+
+// shows info for a specific franchise
+
+async function getTxtFile() {
+  try {
+    let contents = await fs.readFile("franchises.txt", "utf-8");
+    return contents;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getJsonFile() {
+  try {
+    let contents = await fs.readFile("franchiseInfo.json", "utf-8");
+    return contents;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+app.use(express.static("public"));
 const PORT = process.env.PORT || 8000;
 app.listen(PORT);
